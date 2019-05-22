b0a242853e005074c43a0cb1b7a9f39b85d9b420
SithumRavishara <36103578+SithumRavishara@users.noreply.github.com>
2019-05-22 10:03:28 +0530

    Added V6.0

---
+++ V6.0/lib/pages/Product.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'dart:async';
+import '../widgets/ui_elements/title_default.dart';
+import '../scoped-models/main.dart';
+import '../models/product.dart';
+
+class ProductPage extends StatelessWidget {
+  final int productIndex;
+
+  ProductPage(this.productIndex);
+
+  Widget _buildAddressPriceRow(double price) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Food Court,Independent Arcade',
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 5.0),
+          child: Text(
+            '|',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+        Text(
+          '\$' + price.toString(),
+          style: TextStyle(fontFamily: 'Oswald', color: Colors.grey),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(onWillPop: () {
+      print('print backed');
+      Navigator.pop(context, false);
+      return Future.value(false);
+    }, child: ScopedModelDescendant<MainModel>(
+      builder: (BuildContext context, Widget child, MainModel model) {
+        final Product product = model.allProducts[productIndex];
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(product.title),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.network(product.image),
+              Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: TitleDefault(product.title)),
+              _buildAddressPriceRow(product.price),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  product.description,
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    ));
+  }
+}
---
+++ V6.0/lib/scoped-models/connected_products.dart
@@ -0,0 +1,197 @@
+import 'dart:convert';
+import 'dart:async';
+
+import 'package:scoped_model/scoped_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/product.dart';
+import '../models/user.dart';
+
+mixin ConnectedProductsModel on Model {
+  List<Product> _products = [];
+  int _selProductIndex;
+  User _authenticatedUser;
+  bool _isLoading = false;
+
+  Future<Null> addProduct(
+      String title, String description, String image, double price) {
+    _isLoading = true;
+    notifyListeners();
+    final Map<String, dynamic> productData = {
+      'title': title,
+      'description': description,
+      'image':
+          'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/chocolate-fudge-and-salted-caramel-layer-cake.jpg?itok=qYG9yXh8',
+      'price': price,
+      'userEmail': _authenticatedUser.email,
+      'userId': _authenticatedUser.id,
+    };
+    return http
+        .post('https://flutter-products-d1a6b.firebaseio.com/products.json',
+            body: json.encode(productData))
+        .then((http.Response response) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      final Product newProduct = Product(
+          id: responseData['name'],
+          title: title,
+          description: description,
+          image: image,
+          price: price,
+          userEmail: _authenticatedUser.email,
+          userId: _authenticatedUser.id);
+      _products.add(newProduct);
+      _isLoading = false;
+      notifyListeners();
+    });
+  }
+}
+
+mixin ProductsModel on ConnectedProductsModel {
+  bool _showFavorites = false;
+
+  List<Product> get allProducts {
+    //get aka use krnne method()bawitha nokra method akak attrubutes direct access krnna.
+    return List.from(
+        _products); //dn thiyena list aka ntuwa aluthen list space akak thibena list refrnce aktama denwa.
+  }
+
+  List<Product> get displayedProducts {
+    if (_showFavorites) {
+      return _products.where((Product product) => product.isFavorite).toList();
+    }
+    return List.from(_products);
+  }
+
+  int get selectedProductIndex {
+    return _selProductIndex;
+  }
+
+  Product get selectedProduct {
+    if (selectedProductIndex == null) {
+      return null;
+    }
+    return _products[selectedProductIndex];
+  }
+
+  bool get displayFavoritesOnly {
+    return _showFavorites;
+  }
+
+  Future<Null> updateProduct(
+      String title, String description, String image, double price) {
+    _isLoading = true;
+    notifyListeners();
+    final Map<String, dynamic> updateData = {
+      'title': title,
+      'description': description,
+      'image':
+          'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/chocolate-fudge-and-salted-caramel-layer-cake.jpg?itok=qYG9yXh8',
+      'price': price,
+      'userEmail': selectedProduct.userEmail,
+      'userId': selectedProduct.userId
+    };
+    return http
+        .put(
+            'https://flutter-products-d1a6b.firebaseio.com/products/${selectedProduct.id}.json',
+            body: json.encode(updateData))
+        .then((http.Response response) {
+      _isLoading = false;
+      final Product updatedProduct = Product(
+          id: selectedProduct.id,
+          title: title,
+          description: description,
+          image: image,
+          price: price,
+          userEmail: selectedProduct.userEmail,
+          userId: selectedProduct.userId);
+      _products[selectedProductIndex] = updatedProduct;
+      notifyListeners();
+    });
+  }
+
+  void deleteProduct() {
+    _isLoading = true;
+    final deletedProductId = selectedProduct.id;
+    _products.removeAt(selectedProductIndex);
+    _selProductIndex = null;
+    notifyListeners();
+    http
+        .delete(
+            'https://flutter-products-d1a6b.firebaseio.com/products/${deletedProductId}.json')
+        .then((http.Response response) {
+          _isLoading = false;
+          
+    notifyListeners();
+
+        });
+    
+  }
+
+//future<Null> dnne me method aka run wela iwri kiala identify krgena method aka cl krpu thnta notify krnna.
+  Future<Null> fetchProducts() {   
+    _isLoading = true;
+    notifyListeners();
+    return http
+        .get('https://flutter-products-d1a6b.firebaseio.com/products.json')
+        .then((http.Response response) {
+      final List<Product> fetchedProductList = [];
+      final Map<String, dynamic> productListData = json.decode(response.body);
+      if (productListData == null) {
+        _isLoading = false;
+        notifyListeners();
+        return;
+      }
+      productListData.forEach((String productID, dynamic productData) {
+        final Product product = Product(
+            id: productID,
+            title: productData['title'],
+            description: productData['description'],
+            image: productData['image'],
+            price: productData['price'],
+            userEmail: productData['userEmail'],
+            userId: productData['userId']);
+        fetchedProductList.add(product);
+      });
+      _products = fetchedProductList;
+      _isLoading = false;
+      notifyListeners();
+    });
+  }
+
+  void toggleProductFavoriteStatus() {
+    final bool isCurrentlyFavorite = selectedProduct.isFavorite;
+    final bool newFavoriteStatus = !isCurrentlyFavorite;
+    final Product updatedProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        userEmail: selectedProduct.userEmail,
+        userId: selectedProduct.userId,
+        isFavorite: newFavoriteStatus);
+    _products[selectedProductIndex] = updatedProduct;
+    notifyListeners();
+  }
+
+  void selectProduct(int index) {
+    _selProductIndex = index;
+    notifyListeners();
+  }
+
+  void toggleDisplayMode() {
+    _showFavorites = !_showFavorites;
+    notifyListeners();
+  }
+}
+
+mixin UserModel on ConnectedProductsModel {
+  void login(String email, String password) {
+    _authenticatedUser = User(id: 'ascdefg', email: email, password: password);
+  }
+}
+
+mixin UtilityModel on ConnectedProductsModel {
+  bool get isLoading {
+    return _isLoading;
+  }
+}
